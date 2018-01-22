ffe1a444b651c57a4bb24c757b91bccdadb55717
smitaroyal <smita.prabhu@karmagroup.com>
2018-01-22 16:12:08 +0530

    code 22jan

---
+++ contract 22 jan/WebSite5/production/Reports.aspx.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Globalization;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Web.Services;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+using System.Net.Mime;
+using System.Threading;
+using System.ComponentModel;
+
+public partial class WebSite5_production_Country : System.Web.UI.Page
+{
+
+    static string pname;
+    public string getdata()
+    {
+        string user = (string)Session["username"];
+        string htmlstr = "";
+        String conn = ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+        string query = "select distinct parentnode from user_group_access ug join users u on u.[Group Id] = ug.[Group Id]where u.username ='" + user + "' ";
+        SqlConnection sqlcon = new SqlConnection(conn);
+        sqlcon.Open();
+        SqlCommand cmd = new SqlCommand(query, sqlcon);
+        SqlDataReader reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            string name = reader.GetString(0);
+            htmlstr += "<li><a><i class='fa fa-home'></i>" + name + " <span class='fa fa-chevron - down'></span> </a><ul class='nav child_menu'>";
+            SqlConnection sqlcon1 = new SqlConnection(conn);
+            sqlcon1.Open();
+            string query1 = "select * from user_group_access ug join users u on u.[Group Id] =ug.[Group Id]where ug.ParentNode='" + name + "' and  u.username ='" + user + "'";
+            SqlCommand cmd1 = new SqlCommand(query1, sqlcon1);
+
+            SqlDataReader reader1 = cmd1.ExecuteReader();
+            while (reader1.Read())
+            {
+                string pagename = reader1.GetString(1);
+                string pageurl = reader1.GetString(3);
+                string AccessName = reader1.GetString(11);
+
+
+
+
+                htmlstr += "<li><a href=" + pageurl + "?name=" + AccessName + ">" + pagename + " </a></li>";
+                Session["pagename"] = pagename;
+                string office = Queries.GetOffice(user);
+                Session["office"] = office;
+                Session["username"] = user;
+            }
+
+            htmlstr += "</ul></li>";
+
+
+
+            reader1.Close();
+            sqlcon1.Close();
+        }
+        reader.Close();
+        sqlcon.Close();
+     
+        return htmlstr;
+
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        string user = (string)Session["username"];
+        if (user == null)
+        {
+            Response.Redirect("login.aspx");
+        }
+
+        //string user = (string)Session["username"];
+        Label1.Text = "HI!! " + user;
+        Label2.Text = user;
+        string val = getdata();
+    }
+
+   
+
+    [WebMethod]
+    public static void generateReport(string reportName, string reportPro, string date, string office)
+    {
+
+        HttpContext tb = HttpContext.Current;
+        
+
+        //var printr = PrintPdfDropDownList.SelectedItem.Text;
+        DataTable datatable = Queries2.loadDGR(reportPro, date, office);
+
+        ReportDocument crystalReport = new ReportDocument(); // creating object of crystal report
+        crystalReport.Load(tb.Server.MapPath("~/reports/" + reportName + ".rpt")); // path of report       
+        crystalReport.SetDataSource(datatable);
+
+        System.IO.FileStream fs = null;
+        long FileSize = 0;
+        DiskFileDestinationOptions oDest = new DiskFileDestinationOptions();
+        //string ExportFileName = Server.MapPath("~/Copy of holiday_confirm.rpt") + "Export";
+        string ExportFileName = tb.Server.MapPath("~/reports/" + reportName + ".rpt") + "Export";
+        crystalReport.ExportOptions.ExportDestinationType = ExportDestinationType.DiskFile;
+        crystalReport.ExportOptions.ExportFormatType = ExportFormatType.PortableDocFormat;
+        oDest.DiskFileName = ExportFileName;
+        crystalReport.ExportOptions.DestinationOptions = oDest;
+        crystalReport.Export();
+        tb.Response.Clear();
+        tb.Response.Buffer = true;
+        tb.Response.AddHeader("Content-Type", "application/pdf");
+        string fn = reportName + ".pdf";
+        tb.Response.AddHeader("Content-Disposition", "attachment;filename=" + fn + ";");
+
+        fs = new System.IO.FileStream(ExportFileName, FileMode.Open);
+        FileSize = fs.Length;
+        byte[] bBuffer = new byte[Convert.ToInt32(FileSize) + 1];
+        fs.Read(bBuffer, 0, Convert.ToInt32(FileSize));
+        fs.Close();
+        tb.Response.BinaryWrite(bBuffer);
+        tb.Response.Flush();
+
+
+        // Response.Close();
+
+    }
+
+
+
+    public void test(string reportName, string reportPro, string date, string office)
+    {
+        //var printr = PrintPdfDropDownList.SelectedItem.Text;
+        DataTable datatable = Queries2.loadDGR(reportPro, date, office);
+
+        ReportDocument crystalReport = new ReportDocument(); // creating object of crystal report
+        crystalReport.Load(Server.MapPath("~/reports/" + reportName + ".rpt")); // path of report       
+        crystalReport.SetDataSource(datatable);
+
+        System.IO.FileStream fs = null;
+        long FileSize = 0;
+        DiskFileDestinationOptions oDest = new DiskFileDestinationOptions();
+        //string ExportFileName = Server.MapPath("~/Copy of holiday_confirm.rpt") + "Export";
+        string ExportFileName = Server.MapPath("~/reports/" + reportName + ".rpt") + "Export";
+        crystalReport.ExportOptions.ExportDestinationType = ExportDestinationType.DiskFile;
+        crystalReport.ExportOptions.ExportFormatType = ExportFormatType.PortableDocFormat;
+        oDest.DiskFileName = ExportFileName;
+        crystalReport.ExportOptions.DestinationOptions = oDest;
+        crystalReport.Export();
+        Response.Clear();
+        Response.Buffer = true;
+        Response.AddHeader("Content-Type", "application/pdf");
+        string fn = reportName + ".pdf";
+        Response.AddHeader("Content-Disposition", "attachment;filename=" + fn + ";");
+
+        fs = new System.IO.FileStream(ExportFileName, FileMode.Open);
+        FileSize = fs.Length;
+        byte[] bBuffer = new byte[Convert.ToInt32(FileSize) + 1];
+        fs.Read(bBuffer, 0, Convert.ToInt32(FileSize));
+        fs.Close();
+        Response.BinaryWrite(bBuffer);
+        Response.Flush();
+    }
+
+}
